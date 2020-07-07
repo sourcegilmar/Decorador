0a1439d2b1918c33b6392c6d998410a4b798c5dd
sourcegilmar <gilmardw@gmail.com>
2020-07-07 20:29:31 -0300

    Implementado Ingrediente/condimento Leite

--- src/decoradoresconcretos/Leite.java
+++ src/decoradoresconcretos/Leite.java
@@ -1,5 +1,16 @@
 package decoradoresconcretos;
 
-public class Leite {
+import componente.Cafezinho;
+import decorador.DecoradorDeCafe;
 
+public class Leite extends DecoradorDeCafe {
+	Cafezinho cafezinho;
+	
+	public Leite(Cafezinho cafezinho) {
+		this.cafezinho = cafezinho;
+	}
+	
+	public double custo() {
+		return 1.0 + cafezinho.custo();
+	}
 }
