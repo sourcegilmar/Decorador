462b5a514b9a36183f5caca890ba29b6a71ac3db
sourcegilmar <gilmardw@gmail.com>
2020-07-08 13:18:10 -0300

    Implementado classe concrete decorator: Canela

--- src/decoradoresconcretos/Canela.java
+++ src/decoradoresconcretos/Canela.java
@@ -1,5 +1,16 @@
 package decoradoresconcretos;
 
-public class Canela {
+import componente.Cafezinho;
 
+public class Canela {
+	Cafezinho cafezinho;
+	public Canela(Cafezinho cafezinho) {
+		this.cafezinho = cafezinho;
+	}
+	public String getDescricao() {
+		return cafezinho.getDescricao()+", DECORADO COM CANELA.";
+	}
+	public double custo() {
+		return 0.50+cafezinho.custo();
+	}
 }
