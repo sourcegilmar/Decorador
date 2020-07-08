ac36f86e7dd9d4c68c2acad3d03535ed300ab421
sourcegilmar <gilmardw@gmail.com>
2020-07-08 13:04:19 -0300

    Implementado classe Concrete Decorator Cravo

--- src/decoradoresconcretos/Cravo.java
+++ src/decoradoresconcretos/Cravo.java
@@ -1,5 +1,19 @@
 package decoradoresconcretos;
 
+import componente.Cafezinho;
+
 public class Cravo {
+	Cafezinho cafezinho;
+	public Cravo(Cafezinho cafezinho) {
+		this.cafezinho = cafezinho;
+	}
+	
+	public String getDescricao() {
+		return cafezinho.getDescricao()+", DECORADO COM CRAVO.";
+	}
+	
+	public double custo() {
+		return 0.75+cafezinho.custo();
+	}
 
 }
