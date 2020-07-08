96e0000a63c88522598bff663954cc238bc1cc74
sourcegilmar <gilmardw@gmail.com>
2020-07-08 12:50:59 -0300

    Implementado Decorador De Ingrediente/Condimento: Soja

--- src/decoradoresconcretos/Soja.java
+++ src/decoradoresconcretos/Soja.java
@@ -1,5 +1,19 @@
 package decoradoresconcretos;
 
+import componente.Cafezinho;
+
 public class Soja {
+	Cafezinho cafezinho;
+	public Soja(Cafezinho cafezinho) {
+		this.cafezinho = cafezinho;
+	}
+	
+	public String getDescricao() {
+		return cafezinho.getDescricao()+", DECORADO COM SOJA.";
+	}
+	
+	public double custo() {
+		return 1.25+cafezinho.custo();
+	}
 
 }
