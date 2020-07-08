19478efae0d12dc0c040a8db98f363ab6d7e45f9
sourcegilmar <gilmardw@gmail.com>
2020-07-08 12:52:56 -0300

    Implementado classe Ingrediente/Condimento: Chocolate

--- src/decoradoresconcretos/Chocolate.java
+++ src/decoradoresconcretos/Chocolate.java
@@ -1,5 +1,18 @@
 package decoradoresconcretos;
 
-public class Chocolate {
+import componente.Cafezinho;
 
+public class Chocolate {
+	Cafezinho cafezinho;
+	public Chocolate(Cafezinho cafezinho) {
+		this.cafezinho = cafezinho;
+	}
+	
+	public String getDescricao() {
+		return cafezinho.getDescricao()+", DECORADO COM CHOCOLATE.";
+	}
+	
+	public double custo() {
+		return 2.25+cafezinho.custo();
+	}
 }
